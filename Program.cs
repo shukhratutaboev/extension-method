7bb6de2b99e7907f09cb175348751fa4317cda16
shukhratutaboev <shuhratutaboyev2597@gmail.com>
2021-10-19 20:39:02 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using lesson11.Extensions;
+
+namespace lesson11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var name = "156v";
+            // // name = name.Capitalize();
+            // // int a;
+            // try
+            // {
+            //     Console.WriteLine(name.ToInt());
+            // }
+            // catch
+            // {
+            //     Console.WriteLine($"Ey mol son kirit deyildi senga!");
+            // }
+            int a = 781545663;
+            Console.WriteLine(a.Verbalize());
+            
+        }
+    }
+}
